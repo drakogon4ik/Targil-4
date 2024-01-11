a75632f5a7040c8aca711f4424e0e9ae1919fdf6
drakogon4ik <94756197+drakogon4ik@users.noreply.github.com>
2024-01-12 00:05:44 +0200

    Updates

--- ser2.py
+++ ser2.py
@@ -1,17 +1,30 @@
+"""
+Author: Oleg Shkolnik יא9.
+Description: Server that receive GET request from client (site) and send answer:
+             status of request (200 OK / 404 Not Found / 400 Bad Request / 302 Moved Temporarily / 403 Forbidden / 500 Internal Server Error),
+             length of content,
+             type of content,
+             content.
+Date: 11/01/24
+"""
+
+
 import socket
 import os
+from protocol import *
+
 
 HOST = '0.0.0.0'
 PORT = 80
 SITE_FOLDER = 'webroot'
 MOVED_URL = '/index.html'
 SOCKET_TIMEOUT = 2
-specific_urls = {'forbidden', 'moved', 'error'}
-request_error = b"HTTP/1.1 400 Bad Request\n\n<h1>400 Bad Request</h1>"
+specific_urls = ['forbidden', 'moved', 'error']
+request_error = b"HTTP/1.1 400 Bad Request\r\n\r\n<h1>400 Bad Request</h1>"
 types = {
-    'Html': 'text/html;charset=utf-8',
+    'tml': 'text/html;charset=utf-8',
     'css': 'text/css',
-    'js': 'text/javascript; charset=UTF-8',
+    '.js': 'text/javascript; charset=UTF-8',
     'txt': 'text/plain',
     'ico': 'image/x-icon',
     'gif': 'image/jpeg',
@@ -26,21 +39,7 @@ def choosing_type(filename):
     :param filename: file which type we want to know
     :return: type
     """
-    content_type = types['Html']
-    if filename.endswith('css'):
-        content_type = types['css']
-    elif filename.endswith('js'):
-        content_type = types['js']
-    elif filename.endswith('jpg'):
-        content_type = types['jpg']
-    elif filename.endswith('txt'):
-        content_type = types['txt']
-    elif filename.endswith('ico'):
-        content_type = types['ico']
-    elif filename.endswith('gif'):
-        content_type = types['gif']
-    elif filename.endswith('png'):
-        content_type = types['png']
+    content_type = types[filename[-3:]]
     return content_type
 
 
@@ -61,12 +60,40 @@ def searching_url(filename):
     """
     response = b''
     if filename == 'forbidden':
-        response = b"HTTP/1.1 403 Forbidden\n\n<h1>403 Forbidden</h1>"
+        response = b"HTTP/1.1 403 Forbidden\r\n\r\n<h1>403 Forbidden</h1>"
     elif filename == 'moved':
-        response = b"HTTP/1.1 302 Moved Temporarily\r\nLocation: " + bytes(MOVED_URL, 'utf-8') + b"\n\n"
+        response = b"HTTP/1.1 302 Moved Temporarily\r\nLocation: " + bytes(MOVED_URL, 'utf-8') + b"\r\n\r\n"
     elif filename == 'error':
-        response = b"HTTP/1.1 500 Internal Server Error\n\n<h1>500 Internal Server Error</h1>"
+        response = b"HTTP/1.1 500 Internal Server Error\r\n\r\n<h1>500 Internal Server Error</h1>"
     return response
+
+
+def validating_error(request):
+    """
+    function validates if GET request is correct
+    :param request: GET request in type of list
+    :return: true if request is correct and false if not
+    """
+
+    return request[0] == "GET" and request[2] == "HTTP/1.1"
+
+
+def receive_all(sock, def_size=1024):
+    """
+    function make sure that we received all information
+    :param sock: socket of client from which we receiv data
+    :param def_size: default type of socket.recv
+    :return: all data
+    """
+    data = b''
+    while True:
+        chunk = sock.recv(def_size)
+        if not chunk:
+            break
+        data += chunk
+        if len(chunk) < def_size:
+            break
+    return data
 
 
 def serve_file(client_socket, filename):
@@ -80,13 +107,13 @@ def serve_file(client_socket, filename):
         with open(filename, 'rb') as file:
             content = file.read()
             content_type = choosing_type(filename)
-            headers = f"Content-Type: {content_type}\r\nContent-Length: {len(content)}\n\n".encode()
+            headers = f"Content-Type: {content_type}\nContent-Length: {len(content)}\r\n\r\n".encode()
             response = b"HTTP/1.1 200 OK\r\n" + headers + content
             client_socket.sendall(response)
     except FileNotFoundError:
         with open(r'webroot\imgs\error.jpg', 'rb') as file:
             content = file.read()
-            headers = f"Content-Type: {types['jpg']}\r\nContent-Length: {len(content)}\n\n".encode()
+            headers = f"Content-Type: {types['jpg']}\nContent-Length: {len(content)}\r\n\r\n".encode()
             response = b"HTTP/1.1 404 Not Found\r\n" + headers + content
         client_socket.sendall(response)
 
@@ -120,40 +147,42 @@ def main():
                 client_socket.settimeout(SOCKET_TIMEOUT)
                 print(f"Connection established with {addr}")
 
-                request = client_socket.recv(1024).decode()
+                request = receive_all(client_socket).decode()
                 if request:
                     request_split = request.split()
-                    if len(request_split) > 1:
-                        filename = request_split[1][1:]
-                        if filename == '':
-                            filename = 'index.html'
+                    print(request_split)
+                    if validating_error(request_split):
+                        if len(request_split) > 1:
+                            filename = request_split[1][1:]
+                            if filename == '':
+                                filename = 'index.html'
 
-                        filepath = os.path.join(SITE_FOLDER, filename)
+                            filepath = os.path.join(SITE_FOLDER, filename)
 
-                        if "../" not in filename:
-                            """
-                            Specific URLs are checked.
-                            
-                            If the request contains the URL /forbidden, the server sends a "403 Forbidden" error.
-                            
-                            If the request comes to the /error URL, 
-                            the server sends a "500 Internal Server Error" error.
-                            
-                            If the request comes to the /moved URL, 
-                            the server sends "302 Moved Temporarily" as well as the site location.
-                            
-                            Otherwise, the server serves the requested file or sends a "400 Bad Request"
-                            if the request contains an error.
-                            """
+                            if "../" not in filename:
+                                """
+                                Specific URLs are checked.
+                                
+                                If the request contains the URL /forbidden, the server sends a "403 Forbidden" error.
+                                
+                                If the request comes to the /error URL, 
+                                the server sends a "500 Internal Server Error" error.
+                                
+                                If the request comes to the /moved URL, 
+                                the server sends "302 Moved Temporarily" as well as the site location.
+                                
+                                Otherwise, the server serves the requested file or sends a "400 Bad Request"
+                                if the request contains an error.
+                                """
 
-                            if specific(filename):
-                                response = searching_url(filename)
-                                client_socket.sendall(response)
+                                if specific(filename):
+                                    response = searching_url(filename)
+                                    client_socket.sendall(response)
+                                else:
+                                    serve_file(client_socket, filepath)
                             else:
-                                serve_file(client_socket, filepath)
-                        else:
-                            response = request_error
-                            client_socket.sendall(response)
+                                response = request_error
+                                client_socket.sendall(response)
                 else:
                     response = request_error
                     client_socket.sendall(response)
@@ -194,4 +223,6 @@ if __name__ == "__main__":
     assert searching_url('error')
     assert not searching_url('abc')
     assert choosing_type('jpg')
+    assert validating_error(['GET', '/', 'HTTP/1.1', 'headers'])
+    assert not validating_error(['POST', '/', 'HTTP/1.1', 'headers'])
     main()
